7e51ecfbfcd237e29daf0e858a354d67b732c2b1
lll <347552878@qq.com>
2025-10-21 17:06:58 +0800

    style: 添加LightRays美化

--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -1,13 +1,17 @@
 import { ForgotPasswordForm } from '@/components/forgot-password-form'
+import { LightRays } from '@/components/ui/light-rays'
 import { alreadyLoggedRedirect } from '@/hooks/use-redirect'
 
 const ForgotPassword = () => {
   alreadyLoggedRedirect('/')
 
   return (
-    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full px-4'>
-      <ForgotPasswordForm className='max-w-lg  mx-auto' />
-    </div>
+    <>
+      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full px-4'>
+        <ForgotPasswordForm className='max-w-lg  mx-auto' />
+      </div>
+      <LightRays />
+    </>
   )
 }
 
