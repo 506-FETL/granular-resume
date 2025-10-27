ebfe369ae26150884b470ddb15d5894fd7d23520
lll <347552878@qq.com>
2025-10-27 17:30:13 +0800

    fix: 移除构建配置中的 drop_console 选项，保留调试信息以便于开发

--- vite.config.ts
+++ vite.config.ts
@@ -96,7 +96,6 @@ export default defineConfig(({ mode }) => {
       minify: 'terser',
       terserOptions: {
         compress: {
-          drop_console: true,
           drop_debugger: true,
           keep_fnames: false,
           keep_classnames: false,
